b2cf3aa3ddbf21af7fa5ea4dd7ee40a09ef1ee05
evelyn655 <evelyn108061124@gapp.nthu.edu.tw>
2021-06-12 04:09:06 -0700

    06121908 + uart.write

--- final/main.cpp
+++ final/main.cpp
@@ -23,6 +23,8 @@ void RPC_Parking(Arguments *in, Reply *out);
 RPCFunction rpcRPC_Parking(&RPC_Parking, "RPC_Parking");
 void RPC_Line_Following(Arguments *in, Reply *out);
 RPCFunction rpcRPC_Line_Following(&RPC_Line_Following, "RPC_Line_Following");
+void RPC_AprilTag(Arguments *in, Reply *out);
+RPCFunction rpcRPC_AprilTag(&RPC_AprilTag, "RPC_AprilTag");
 
 void encoder_control() {
    int value = encoder;
@@ -31,89 +33,16 @@ void encoder_control() {
 }
 
 void RPC_Parking(Arguments *in, Reply *out)   {
-    // double d1 = in->getArg<double>();
-    // double d2 = in->getArg<double>();
-    // char face = in->getArg<char>();
-
     car.turn(50, -0.3);
     ThisThread::sleep_for(2500ms);
     car.stop();
-
-    // if (face=='S') {
-    //     // go back d2 cm
-    //     encoder_ticker.attach(&encoder_control, 10ms);
-    //     steps = 0;
-    //     last = 0;
-    //     car.goStraight(-50);
-
-    //     printf("d1 = %f\n", d1);
-    //     printf("d2 = %f\n", d2);
-    //     printf("face = %c\n", face);
-
-    //     while(steps*6.5*3.14/32 < (d2-5+7)) {
-    //         printf("encoder = %d\r\n", steps);
-    //         ThisThread::sleep_for(100ms);
-    //     }
-    //     car.stop();
-    // } else {
-    //     // go back d1 cm
-    //     encoder_ticker.attach(&encoder_control, 10ms);
-    //     steps = 0;
-    //     last = 0;
-    //     car.goStraight(-50);
-
-    //     printf("d1 = %f\n", d1);
-    //     printf("d2 = %f\n", d2);
-    //     printf("face = %c\n", face);
-
-    //     while(steps*6.5*3.14/32 < (d1-5+17)) {
-    //         printf("encoder = %d\r\n", steps);
-    //         ThisThread::sleep_for(100ms);
-    //     }
-    //     car.stop();
-    //     ThisThread::sleep_for(3000ms);
-
-        
-    //     // turning 
-    //     if (face=='U') {
-    //         car.turn(50, 0.3);
-    //         ThisThread::sleep_for(2500ms);
-    //         car.stop();
-    //     } else if (face=='D') {
-    //         car.turn(50, -0.3);
-    //         ThisThread::sleep_for(2800ms);
-    //         car.stop();
-    //     }
-    //     ThisThread::sleep_for(3000ms);
-        
-    //     // go back d2 cm
-    //     encoder_ticker.attach(&encoder_control, 10ms);
-    //     steps = 0;
-    //     last = 0;
-    //     car.goStraight(-50);
-
-    //     printf("d1 = %f\n", d1);
-    //     printf("d2 = %f\n", d2);
-    //     printf("face = %c\n", face);
-
-    //     while(steps*6.5*3.14/32 < (d2-5+15.5+11)) {
-    //         printf("encoder = %d\r\n", steps);
-    //         ThisThread::sleep_for(100ms);
-    //     }
-    //     car.stop();
-
-    // }
-
-
-
-    //car.goCertain_Distance(d1);
-    
-    //car.turn(speed,turn);
     return;
 }
 
-
 void RPC_Line_Following(Arguments *in, Reply *out) {
+    // char ch[1] = {'L'};
+    // uart.write(ch, sizeof(ch));
+    // printf("write ch = %c", ch[0]);
     while(1){
         printf("while loop\r\n");
         //printf("uart = %d\r\n", uart.readable());
@@ -137,10 +66,6 @@ void RPC_Line_Following(Arguments *in, Reply *out) {
                 car.goStraight(50);
                 ThisThread::sleep_for(500ms);
                 car.stop();
-                
-                
-                // ping detection to stop the car
-                // or at the end, break the while loop?
 
             } else if (recv[0] == '0') {   // stop
                 printf("stop\r\n");
@@ -152,7 +77,25 @@ void RPC_Line_Following(Arguments *in, Reply *out) {
    }
 }
 
+void RPC_AprilTag(Arguments *in, Reply *out)   {
+    char ch[1] = {'A'};
+    uart.write(ch, sizeof(ch));
+    printf("write ch = %c", ch[0]);
 
+
+
+
+
+
+
+
+
+    
+    // car.turn(50, -0.3);
+    // ThisThread::sleep_for(2500ms);
+    // car.stop();
+    return;
+}
 
 int main() {
     //pc.set_baud(9600);
